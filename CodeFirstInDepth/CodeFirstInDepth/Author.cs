72a0fd1c3555699cdd981eddb0429a6a0192d637
Paddy Mc Glone <patrick.mcglone@civica.co.uk>
2019-08-21 14:52:43 +0100

    Adding the classes needed

---
+++ CodeFirstInDepth/CodeFirstInDepth/Author.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CodeFirstInDepth
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // A one-to-many relationship between author and courses
+        public IList<Course> Courses { get; set; }
+    }
+}
---
+++ CodeFirstInDepth/CodeFirstInDepth/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstInDepth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
