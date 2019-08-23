e7235561800267e3325d128c2fc9f1663a384c57
Paddy Mc Glone <patrick.mcglone@civica.co.uk>
2019-08-23 11:22:38 +0100

    Create Category.cs
    - Adding Id and first property

---
+++ CodeFirstInDepth/CodeFirstInDepth/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstInDepth
+{
+    public class Category
+    {
+        public int Category_Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
