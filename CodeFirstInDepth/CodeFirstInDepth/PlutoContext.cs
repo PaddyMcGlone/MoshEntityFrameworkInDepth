45305cf75eb46c1e4bf319c1edaf6d2c630ce8d0
Paddy Mc Glone <patrick.mcglone@civica.co.uk>
2019-08-23 08:58:22 +0100

    Create PlutoContext.cs
    - Adding the DbContext

---
+++ CodeFirstInDepth/CodeFirstInDepth/PlutoContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace CodeFirstInDepth
+{
+    public class PlutoContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
